03c3ffbc4f565d31e2457f0d64430956d6fc8bc0
algebraic-designer-6218 <jmiller@archersystems.us>
2019-11-20 18:45:43 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,15 @@
+artist =["array"]
+next_artist = "string"
+artists.push(next_artist)
+
+artist =["array"]
+next_artist = "string"
+artists.unshift(next_artist)
+
+artist =["array"]
+next_artist = "string"
+artists.pop(next_artist)
+
+artist =["array","string", "number"]
+next_artist = "string"
+artists.push(next_artist)
